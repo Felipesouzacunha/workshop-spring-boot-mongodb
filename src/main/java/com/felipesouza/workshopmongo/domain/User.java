412ad480657ca7d87b46453d81713313eee4b7b0
Felipe de Souza <131294693+Felipesouzacunha@users.noreply.github.com>
2025-02-08 15:34:14 -0300

    Operação de instanciação da base de dados

--- src/main/java/com/felipesouza/workshopmongo/domain/User.java
+++ src/main/java/com/felipesouza/workshopmongo/domain/User.java
@@ -3,7 +3,6 @@ package com.felipesouza.workshopmongo.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
