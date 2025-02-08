9e10743c427fa2e4e4efeaa852bcdefd1e623421
Felipe de Souza <131294693+Felipesouzacunha@users.noreply.github.com>
2025-02-08 18:13:50 -0300

    Inserção de usuário com POST

--- src/main/java/com/felipesouza/workshopmongo/services/UserService.java
+++ src/main/java/com/felipesouza/workshopmongo/services/UserService.java
@@ -1,11 +1,13 @@
 package com.felipesouza.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipesouza.workshopmongo.domain.User;
+import com.felipesouza.workshopmongo.dto.UserDTO;
 import com.felipesouza.workshopmongo.repository.UserRepository;
 import com.felipesouza.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -21,12 +23,15 @@ public class UserService {
 
 	public User findById(String id) {
 		
-		User obj = repository.findById(id).orElse(null);
-		
-		if(obj == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado!");
-		}
-		return obj;
-
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getNome(), objDto.getEmail());
 	}
 }
