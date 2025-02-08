ca9e234d210f2e8e0613e9abf83fa5cff4bbbe9f
Felipe de Souza <131294693+Felipesouzacunha@users.noreply.github.com>
2025-02-08 19:01:26 -0300

    Atualização de usuário

--- src/main/java/com/felipesouza/workshopmongo/services/UserService.java
+++ src/main/java/com/felipesouza/workshopmongo/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.felipesouza.workshopmongo.domain.User;
 import com.felipesouza.workshopmongo.dto.UserDTO;
 import com.felipesouza.workshopmongo.repository.UserRepository;
+import com.felipesouza.workshopmongo.services.exception.EntityNotFoundException;
 import com.felipesouza.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
@@ -22,26 +23,43 @@ public class UserService {
 	}
 
 	public User findById(String id) {
-		
+
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.insert(obj);
+	}
+
+	public void delete(String id) {
+		try {
+		User user = findById(id);
+		repository.delete(user);
+		} catch (EntityNotFoundException e) {
+			throw new ObjectNotFoundException("Objeto não encontrado!");
+		}
+	}
+
+	public User update(String id, User obj) {
+
+		try {
+			User user = findById(id);
+			updateData(user, obj);
+			return repository.save(user);
+		} catch (EntityNotFoundException e) {
+			throw new ObjectNotFoundException("Objeto não encontrado! ");
+		}
+
+	}
+
+	private void updateData(User userEntity, User obj) {
+		userEntity.setName(obj.getName());
+		userEntity.setEmail(obj.getEmail());
 	}
 	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getNome(), objDto.getEmail());
 	}
-	
-	public void delete(String id) {
-		User user = findById(id);
-		repository.delete(user);
-	}
-	
-	
-	
-	
-	
+
 }
---
+++ src/main/java/com/felipesouza/workshopmongo/services/exception/EntityNotFoundException.java
@@ -0,0 +1,11 @@
+package com.felipesouza.workshopmongo.services.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public EntityNotFoundException(String msg) {
+		super(msg);
+	}
+	
+}
