df22d47d4061af36ca69ac29b2991d32d820d9d0
Felipe de Souza <131294693+Felipesouzacunha@users.noreply.github.com>
2025-02-08 22:59:10 -0300

    Consulta simples com query methods

--- src/main/java/com/felipesouza/workshopmongo/services/PostService.java
+++ src/main/java/com/felipesouza/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.felipesouza.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,7 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
-	
+	public List<Post> findByTitle(String text){
+		return postRepository.findByTitleContainingIgnoreCase(text);
+	}
 }
